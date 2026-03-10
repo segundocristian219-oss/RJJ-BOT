e101e44000653dff7ce0ae78e0cf11ff4d19e409
segundocristian219-oss <segundocristian219@gmail.com>
2026-03-09 22:52:48 -0600

    Crear kalahari.js

---
+++ plugins/grupos/kalahari.js
@@ -0,0 +1,30 @@
+let handler = async (m, { isPrems, conn }) => {
+let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
+if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
+
+let img = 'https://files.catbox.moe/otc1fd.jpg' 
+let texto = `» 𝐌𝐀𝐏𝐀 𝐊𝐀𝐋𝐀𝐇𝐀𝐑𝐈 𝐅𝐑𝐄𝐄 𝐅𝐈𝐑𝐄 ✅`
+
+const fkontak = {
+        "key": {
+    "participants":"0@s.whatsapp.net",
+                "remoteJid": "status@broadcast",
+                "fromMe": false,
+                "id": "Halo"
+        },
+        "message": {
+                "contactMessage": {
+                        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+                }
+        },
+        "participant": "0@s.whatsapp.net"
+}
+await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
+global.db.data.users[m.sender].lastcofre = new Date * 1
+}
+handler.help = ['kalahari']
+handler.tags = ['freefire']
+handler.command = ['kalahari'] 
+handler.register = false
+handler.admin = true
+export default handler
