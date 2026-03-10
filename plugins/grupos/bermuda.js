86d4e982e3d2d4ae7ee30605e24dbc3a81447b4b
segundocristian219-oss <segundocristian219@gmail.com>
2026-03-09 22:49:14 -0600

    Actualizar bermuda.js

--- plugins/grupos/bermuda.js
+++ plugins/grupos/bermuda.js
@@ -2,7 +2,7 @@ let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-let img = 'https://qu.ax/kNeVW.jpg' 
+let img = 'https://files.catbox.moe/tcti0i.jpg' 
 let texto = `» 𝙈𝘼𝙋𝘼 𝘽𝙀𝙍𝙈𝙐𝘿𝘼 𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀 ✅`
 
 const fkontak = {
@@ -24,7 +24,7 @@ global.db.data.users[m.sender].lastcofre = new Date * 1
 }
 handler.help = ['bermuda']
 handler.tags = ['freefire']
-handler.command = ['nexterra'] 
+handler.command = ['bermuda'] 
 handler.register = false
 handler.admin = true
 export default handler
