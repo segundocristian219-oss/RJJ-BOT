f76a6b39cf39a6242123b8c7fae2c0464b218ade
segundocristian219-oss <segundocristian219@gmail.com>
2026-03-09 22:39:36 -0600

    Actualizar menu.js

--- plugins/menus/menu.js
+++ plugins/menus/menu.js
@@ -145,7 +145,7 @@ const handler = async (m, { conn }) => {
   await conn.sendMessage(
     m.chat,
     {
-      video: { url: "https://files.catbox.moe/qik0ad.mp4" },
+      video: { url: "https://files.catbox.moe/z43302.mp4" },
       caption: menu,
       gifPlayback: true,
       mentions: [m.sender]
