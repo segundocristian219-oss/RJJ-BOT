4bb3cc3f158ec00fc5ab63ef4af355d9f37eb6bd
segundocristian219-oss <segundocristian219@gmail.com>
2026-03-11 23:29:43 -0600

    Actualizar mute.js

--- plugins/grupos/mute.js
+++ plugins/grupos/mute.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 import fs from 'fs/promises'
 
-const OWNER_LID = '217158512549931@lid''230008920490230@lid'
+const OWNER_LID = '217158512549931@lid' 
 const DB_DIR = './database'
 const DATA_FILE = `${DB_DIR}/muted.json`
 
